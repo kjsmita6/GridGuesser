710c1f1043acd84cfb458f2a6ca55f9f2d0b3ec2
Michael Bosik <michaelbosik@gmail.com>
2020-10-08 17:38:09 -0400

    option screen and light sensor data

--- app/GridGuesser/app/src/main/java/com/example/gridguesser/GameActivity.kt
+++ app/GridGuesser/app/src/main/java/com/example/gridguesser/GameActivity.kt
@@ -1,11 +1,56 @@
 package com.example.gridguesser
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class GameActivity : AppCompatActivity() {
+private const val TAG = "SensorData"
+
+class GameActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var pressure: Sensor? = null
+    private lateinit var bg: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        bg = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.gameBackground)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        pressure = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        val light = event.values[0]
+        Log.d(TAG, light.toString())
+        if (light < 20) {
+            Log.d(TAG, "Light levels below 20. Switching to dark theme")
+            bg.rootView.setBackgroundColor(resources.getColor(android.R.color.darker_gray))
+        }
+        else {
+            Log.d(TAG, "Light levels above 20. Switching to light theme")
+            bg.rootView.setBackgroundColor(resources.getColor(android.R.color.white))
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }
