57c8279a1817ee185ddc7a79781868b74332a40f
nwalzer <nswalzer@gmail.com>
2020-10-11 21:47:54 -0400

    Rudimentary firebase message receiving and notification sending set up

--- app/GridGuesser/app/src/main/java/com/example/gridguesser/GridGuesserApplication.kt
+++ app/GridGuesser/app/src/main/java/com/example/gridguesser/GridGuesserApplication.kt
@@ -1,8 +1,12 @@
 package com.example.gridguesser
 
 import android.app.Application
+import android.util.Log
+import android.widget.Toast
 import com.example.gridguesser.database.GameRepository
 import com.example.gridguesser.http.ServerInteractions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 private const val TAG = "GridGuesser"
 
--- app/GridGuesser/app/src/main/java/com/example/gridguesser/MainActivity.kt
+++ app/GridGuesser/app/src/main/java/com/example/gridguesser/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.gridguesser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.gridguesser.activegames.ActiveGamesActivity
 import com.example.gridguesser.database.GameRepository
 import com.example.gridguesser.deviceID.DeviceID
 import com.example.gridguesser.http.ServerInteractions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+import java.lang.Exception
+
 
 private const val TAG = "GridGuesser"
 
@@ -23,29 +29,40 @@ class MainActivity : AppCompatActivity() {
         deviceID = DeviceID.getDeviceID(contentResolver)
 
         Log.d(TAG, deviceID)
+
+        val token = FirebaseInstanceId.getInstance().token
+        Log.d(TAG, "Registration Token: = $token")
+
         serverInteractions = ServerInteractions.get()
-        serverInteractions.serverStatus.observe(
-            this,
-            Observer { responseString ->
-                Log.d(TAG, "Response received: $responseString")
-            })
+        try {
+            serverInteractions.serverStatus.observe(
+                this,
+                Observer { responseString ->
+                    Log.d(TAG, "Response received: $responseString")
+                })
+        } catch (e: Exception){
+            Log.e(TAG, e.toString())
+        }
 
         settingsRepo = GameRepository.get()
-        settingsRepo.getSettings().observe(
-            this,
-            Observer { responseList ->
-                if(responseList.isEmpty()){
-                    settingsRepo.addSettings(settingsRepo.currentSettings)
-                    findViewById<TextView>(R.id.user_welcome).text = "Welcome, ${settingsRepo.currentSettings.username}"
-                    serverInteractions.addUser(deviceID)
-
-                } else {
-                    settingsRepo.currentSettings = responseList[0]
-                    findViewById<TextView>(R.id.user_welcome).text = "Welcome, ${responseList[0].username}"
+        try {
+            settingsRepo.getSettings().observe(
+                this,
+                Observer { responseList ->
+                    if(responseList.isEmpty()){
+                        settingsRepo.addSettings(settingsRepo.currentSettings)
+                        findViewById<TextView>(R.id.user_welcome).text = "Welcome, ${settingsRepo.currentSettings.username}"
+                        serverInteractions.addUser(deviceID)
+                    } else {
+                        settingsRepo.currentSettings = responseList[0]
+                        findViewById<TextView>(R.id.user_welcome).text = "Welcome, ${responseList[0].username}"
+                    }
+                    Log.d(TAG, "FOUND SETTINGS: $responseList")
                 }
-                Log.d(TAG, "FOUND SETTINGS: $responseList")
-            }
-        )
+            )
+        } catch (e: Exception){
+            Log.e(TAG, e.toString())
+        }
 
         findViewById<Button>(R.id.active_games)?.setOnClickListener {
             activeGames()
